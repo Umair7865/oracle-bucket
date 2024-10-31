12819b1f9134bb580a4639b8fe6fd014b6a86148
Syed Umair Ali Gillani <87699636+Umair-Gillani@users.noreply.github.com>
2024-10-31 18:45:07 +0500

    umair

--- backup-in-tar.sh
+++ backup-in-tar.sh
@@ -3,21 +3,76 @@
 # Define variables
 ARCHIVE_NAME="backup_mysql.tar.gz"
 DESTINATION_FOLDER="$HOME/backup-in-tar"          # DESTINATION_FOLDER="/home/opc/opc/seperate-mysql-db-bk/backup_tar"
-SOURCE_FOLDER="< ENTER /path/of/backups >"        # Enter the complete path of parent directory which is storing all database backup in seperate folder
+SOURCE_FOLDER="$HOME/MySQL"        # Enter the complete path of parent directory which is storing all database backup in seperate folder
                                                   # means if there is 2 database -> create 2 folder with the EXACT name of database and store dump into 
                                                   # relvant directory
 
 # making downloads directory if not exist 
-DOWNLOAD_DIR="$HOME/backup-in-tar"
+MySQL_DIR="$HOME/MySQL"
 
 # Check if the directory exists
-if [ ! -d "$DOWNLOAD_DIR" ]; then
+if [ ! -d "$MySQL_DIR" ]; then
     # If not, create the directory
-    mkdir "$DOWNLOAD_DIR"
-    echo "Directory 'backup-in-tar' created at $HOME"
+    mkdir "$MySQL_DIR"
+    echo "Directory 'MySQL' created at $HOME"
 else
+    echo "Directory 'MySQL' already exists at $HOME"
+fi
+
+
+echo "Please organize your backup directory as follows:"
+echo "
+$HOME/MySQL
+│
+├── database1/
+│   └── dump.sql       # Backup file for database1
+│
+├── database2/
+│   └── dump.sql       # Backup file for database2
+│
+└── database3/
+    └── dump.sql       # Backup file for database3
+"
+
+echo "Note:"
+echo "1. Place each database directory under $HOME/MySQL"
+echo "2. Each directory must be named exactly after the corresponding database (e.g., database1, database2)."
+echo "3. Place each dump.sql file inside its respective database folder."
+echo " "
+echo " "
+
+
+
+# Prompt for confirmation before proceeding
+read -p "Arrange the files as shown in above diagram than run this script to create TAR file. After arranging database path as per diagram confirm with (yes/no): " confirmation
+
+if [[ "$confirmation" != "yes" ]]; then
+    echo " "
+    echo "Take your Time and Arrange it Accordingly."
+    echo " "
+    exit 1
+fi
+
+
+# making downloads directory if not exist 
+BACKUP_TAR="$HOME/backup-in-tar"
+
+# Check if the directory exists
+if [ ! -d "$BACKUP_TAR" ]; then
+    # If not, create the directory
+    mkdir "$BACKUP_TAR"
+    echo " "
+    echo "Directory 'backup-in-tar' created at $HOME"
+    echo " "
+else
+    echo " "
     echo "Directory 'backup-in-tar' already exists at $HOME"
+    echo " "
 fi
 
 # Archive the folder
 tar -czf $DESTINATION_FOLDER/$ARCHIVE_NAME -C $SOURCE_FOLDER .
+
+echo " "
+echo " Congratulations!!! TAR file of your database has been created under directory $HOME/backup-in-tar"
+echo " "
